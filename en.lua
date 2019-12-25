fd0d4b37eae07e9f97fcd3e9787b24b2186b1f1e
MeaslyStorm RP <48359310+measlystorm@users.noreply.github.com>
2019-12-24 19:07:30 -0500

    added en config file
    added en config file for more requests please join my discord thanks.

---
+++ en.lua
@@ -0,0 +1,17 @@
+Locales['en'] = {
+	['uploads_to_parses_oranges'] = 'Press ~INPUT_CONTEXT~ to pick fresh oranges',
+	['load_to_pick_abras_of_orange'] = 'Press ~INPUT_CONTEXT~ to make the fresh orange juice',
+	['load_to_sell_summary_summary'] = 'Press ~INPUT_CONTEXT~ to sell fresh orange juice',
+	['mochila_full'] = 'You cant pick oranges anymore because your backpack is full ~r~full~s~',
+	['pick_oranges'] = '~y~Picking Oranges~s~...',
+	['not enough_arrangements'] = 'You dont have enough oranges',
+	['no_more_thanks'] = 'You Have No More ~r~Oranges~s~',
+	['transform_summary'] = '~y~Turning To Fresh Orange Juice~s~...',
+	['orange_summary_tems'] = 'You Have No More ~r~Orange juice~s~',
+	['vendeste_sumo'] = 'you sold ~y~x1~s~ ~y~Orange juice~s~',
+	['sale_of_summary'] = '~g~Selling Fresh Orange Juice~s~...',
+	-- Blips
+	['orange_catch'] = 'Oranges Picking Zone',
+	['orange_ transform'] = 'Process For Orange Sumo',
+	['orange_summary_sale'] = 'Selling Orange Juice',
+}
